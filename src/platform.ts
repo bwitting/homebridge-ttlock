8abd08b5ac1b90c53f1a7a7f1025d0fbe7e33ffa
Bronson Witting <bwitting@outlook.com>
2022-08-29 15:38:39 +0000

    Added handling for invalid or no  API/user creds

--- src/platform.ts
+++ src/platform.ts
@@ -55,83 +55,93 @@ export class TtlockPlatform implements DynamicPlatformPlugin {
    */
   async discoverDevices() {
 
-    const theAccessToken = await this.apiClient.getAccessTokenAsync();
-    let response;
-
-    // Sends the HTTP request to get the locks on the account
     try {
-      const now = new Date().getTime();
-      response = await axios.post<LocksResponse>('https://euapi.ttlock.com/v3/lock/list', qs.stringify({
-        clientId: this.config.clientid,
-        accessToken: theAccessToken,
-        pageNo:  1,
-        pageSize: 100,
-        date: now,
-      }), {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-      this.log.debug('Number of locks returned from API: ' + String(response.data.list.length));
+      const theAccessToken = await this.apiClient.getAccessTokenAsync();
+      let response;
 
-
-    } catch (e) {
-      this.log.warn(`Error while getting status via API: ${e}`);
-    }
-
-
-    // loop over the discovered devices and register each one if it has not already been registered
-    if (response.data.list.length > 0) {
+      // Sends the HTTP request to get the locks on the account
       try {
-        response.data.list.forEach(device => {
-          // generate a unique id for the accessory this should be generated from
-          // something globally unique, but constant, for example, the device serial
-          // number or MAC address
-          const uuid = this.api.hap.uuid.generate(String(device.lockId));
+        const now = new Date().getTime();
+        response = await axios.post<LocksResponse>('https://euapi.ttlock.com/v3/lock/list', qs.stringify({
+          clientId: this.config.clientid,
+          accessToken: theAccessToken,
+          pageNo:  1,
+          pageSize: 100,
+          date: now,
+        }), {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
+        if (response.data.errcode === null || response.data.errcode === undefined) {
+          this.log.debug('Number of locks returned from API: ' + String(response.data.list.length));
+        } else {
+          this.log.error('Ensure your TTLock API keys and username/password are correct in the config. Error: ' + response.data.errcode);
+        }
 
-          // see if an accessory with the same uuid has already been registered and restored from
-          // the cached devices we stored in the `configureAccessory` method above
-          const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
-          if (existingAccessory) {
-            // the accessory already exists
-            this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-            // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-            // existingAccessory.context.device = device;
-            // this.api.updatePlatformAccessories([existingAccessory]);
-
-            // create the accessory handler for the restored accessory
-            // this is imported from `platformAccessory.ts`
-            new TtlockPlatformAccessory(this, existingAccessory);
-
-            // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-            // remove platform accessories when no longer present
-            // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-            // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-          } else {
-            // the accessory does not yet exist, so we need to create it
-            this.log.info('Adding new accessory:', device.lockAlias);
-
-            // create a new accessory
-            const accessory = new this.api.platformAccessory(device.lockAlias, uuid);
-
-            // store a copy of the device object in the `accessory.context`
-            // the `context` property can be used to store any data about the accessory you may need
-            accessory.context.device = device;
-
-            // create the accessory handler for the newly create accessory
-            // this is imported from `platformAccessory.ts`
-            new TtlockPlatformAccessory(this, accessory);
-
-            // link the accessory to your platform
-            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-          }
-        },
-        );
       } catch (e) {
-        this.log.warn(`Error while adding devices: ${e}`);
+        this.log.error(`Error while getting locks via API: ${e}`);
       }
+
+
+      // loop over the discovered devices and register each one if it has not already been registered
+      if (response.data.errcode === null || response.data.errcode === undefined) {
+        if (response.data.list.length > 0) {
+          try {
+            response.data.list.forEach(device => {
+              // generate a unique id for the accessory this should be generated from
+              // something globally unique, but constant, for example, the device serial
+              // number or MAC address
+              const uuid = this.api.hap.uuid.generate(String(device.lockId));
+
+              // see if an accessory with the same uuid has already been registered and restored from
+              // the cached devices we stored in the `configureAccessory` method above
+              const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+
+              if (existingAccessory) {
+                // the accessory already exists
+                this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+
+                // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
+                // existingAccessory.context.device = device;
+                // this.api.updatePlatformAccessories([existingAccessory]);
+
+                // create the accessory handler for the restored accessory
+                // this is imported from `platformAccessory.ts`
+                new TtlockPlatformAccessory(this, existingAccessory);
+
+                // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
+                // remove platform accessories when no longer present
+                // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+                // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+              } else {
+                // the accessory does not yet exist, so we need to create it
+                this.log.info('Adding new accessory:', device.lockAlias);
+
+                // create a new accessory
+                const accessory = new this.api.platformAccessory(device.lockAlias, uuid);
+
+                // store a copy of the device object in the `accessory.context`
+                // the `context` property can be used to store any data about the accessory you may need
+                accessory.context.device = device;
+
+                // create the accessory handler for the newly create accessory
+                // this is imported from `platformAccessory.ts`
+                new TtlockPlatformAccessory(this, accessory);
+
+                // link the accessory to your platform
+                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+              }
+            },
+            );
+          } catch (e) {
+            this.log.error(`Error while adding devices: ${e}`);
+          }
+        }
+      }
+    } catch (e) {
+      this.log.error(`Error: ${e}. Ensure your API keys and username/password are correct in the config.`);
     }
   }
 }
